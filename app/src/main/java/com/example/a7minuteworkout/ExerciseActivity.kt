f275c62da38ee33882f0caecd35940c047989950
daniel shelton <danielshelton90@gmail.com>
2021-09-14 08:34:17 -0700

    created Finish screen

--- app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7minuteworkout
 
+import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -112,7 +113,9 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                     exerciseAdapter!!.notifyDataSetChanged()
                     setupRestView()
                 } else {
-                    Toast.makeText(this@ExerciseActivity, "Congratulations! You have completed the 7 minutes workout.", Toast.LENGTH_SHORT).show()
+                    finish()
+                    val intent = Intent(this@ExerciseActivity, FinishActivity::class.java)
+                    startActivity(intent)
                 }
             }
         }.start()
