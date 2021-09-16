115a24cefa3a5ecced6c01d967151dbc8ed76464
daniel shelton <danielshelton90@gmail.com>
2021-09-15 21:39:59 -0700

    preparing for dates listView

--- app/src/main/java/com/example/a7minuteworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/HistoryActivity.kt
@@ -2,6 +2,7 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_history.*
 
 class HistoryActivity : AppCompatActivity() {
@@ -18,5 +19,18 @@ class HistoryActivity : AppCompatActivity() {
         toolbar_history_activity.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        getAllCompletedDates()
+
+    }
+
+    private fun getAllCompletedDates() {
+        val dbHandler = SqliteOpenHelper(this, null)
+        val allCompletedDatesList = dbHandler.getAllCompletedDatesList()
+
+        for (i in allCompletedDatesList) {
+            Log.i("DateHISTORYACTIVITY", "" + i)
+        }
+
     }
 }
