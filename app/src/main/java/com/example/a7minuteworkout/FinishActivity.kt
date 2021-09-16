3951847253fd768edbdd111340c6a79be785f711
daniel shelton <danielshelton90@gmail.com>
2021-09-15 21:24:22 -0700

    using SQLite for storing workout sessions

--- app/src/main/java/com/example/a7minuteworkout/FinishActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/FinishActivity.kt
@@ -2,7 +2,10 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_finish.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,5 +26,22 @@ class FinishActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        addDateToDatabase()
+
     }
+
+    private fun addDateToDatabase() {
+        val calendar = Calendar.getInstance()
+        val dateTime = calendar.time
+        Log.i("DATE:", "" + dateTime)
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+
+        val dbHandler = SqliteOpenHelper(this, null)
+        dbHandler.addDate(date)
+        Log.i("DATE: ", "ADDED")
+    }
+
+
 }
