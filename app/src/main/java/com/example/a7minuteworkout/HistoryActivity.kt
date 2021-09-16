b7edbf333465785e5ebc025de4ec5f971c4c329b
daniel shelton <danielshelton90@gmail.com>
2021-09-15 22:02:43 -0700

    populate history recycler view

--- app/src/main/java/com/example/a7minuteworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/HistoryActivity.kt
@@ -3,6 +3,8 @@ package com.example.a7minuteworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_history.*
 
 class HistoryActivity : AppCompatActivity() {
@@ -28,8 +30,18 @@ class HistoryActivity : AppCompatActivity() {
         val dbHandler = SqliteOpenHelper(this, null)
         val allCompletedDatesList = dbHandler.getAllCompletedDatesList()
 
-        for (i in allCompletedDatesList) {
-            Log.i("DateHISTORYACTIVITY", "" + i)
+        if (allCompletedDatesList.size > 0) {
+            tvHistory.visibility = View.VISIBLE
+            rvHistory.visibility = View.VISIBLE
+            tvNoDataAvailable.visibility = View.GONE
+
+            rvHistory.layoutManager = LinearLayoutManager(this)
+            val historyAdapter = HistoryAdapter(this, allCompletedDatesList)
+            rvHistory.adapter = historyAdapter
+        } else {
+            tvHistory.visibility = View.GONE
+            rvHistory.visibility = View.GONE
+            tvNoDataAvailable.visibility = View.VISIBLE
         }
 
     }
